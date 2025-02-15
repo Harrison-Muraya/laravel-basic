bbe612b79ce74ca931b09c95d63077761c13449c
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-02-16 01:29:48 +0300

    slot

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <x-nav-link href="/">home</x-nav-link>
+        <x-nav-link href="/about">about</x-nav-link>
+        <x-nav-link href="/contact">contact</x-nav-link>
+
+        {{-- <a href="/">home</a>
+        <a href="/about">about</a>
+        <a href="/contact">contact</a> --}}
+    </nav>
+    {{ $slot }}
+</body>
+</html>
