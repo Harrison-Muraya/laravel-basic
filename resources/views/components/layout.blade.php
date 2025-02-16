79c6ed07f5e8965d3fb7006b2094383509fb3093
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-02-16 03:28:17 +0300

    homework  on string prop active

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,7 +23,7 @@
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <x-nav-link href="/" :active="(request()->is('/'))">home</x-nav-link>
                 <x-nav-link href="/about" :active="(request()->is('about'))" >about</x-nav-link>
-                <x-nav-link href="/contact" :active="(request()->is('contact'))">contact</x-nav-link>
+                <x-nav-link href="/contact" :active="(request()->is('contact'))" type="button">contact</x-nav-link>
               </div>
             </div>
           </div>
