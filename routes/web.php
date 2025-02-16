6ce820b1af5f5d0c6ce2f4311e1fafda928effab
Harrison-Muraya <112485035+Harrison-Muraya@users.noreply.github.com>
2025-02-16 05:15:45 +0300

    Autoloading, namespacing and models

--- routes/web.php
+++ routes/web.php
@@ -2,55 +2,65 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\job;
+
 
 Route::get('/', function () {
     return view('home');
 });
 Route::get('/jobs', function () {
     return view('jobs',[
-        'jobs'=>[
-            [
-                'id'=> 1,
-                'title'=>'programmer',
-                'salary'=> 300000,
-            ],
-            [
-                'id'=> 2,
-                'title'=>'techear',
-                'salary'=> 3290000,
-            ],
-            [
-                'id'=> 3,
-                'title'=>'engineer',
-                'salary'=> 4000000,
-            ]
-        ]
+        'jobs'=> job::all()
     ]);
 });
 Route::get('/job/{id}', function ($id) {
-    $jobs= [
-        [
-            'id'=> 1,
-            'title'=>'programmer',
-            'salary'=> 300000,
-        ],
-        [
-            'id'=> 2,
-            'title'=>'techear',
-            'salary'=> 3290000,
-        ],
-        [
-            'id'=> 3,
-            'title'=>'engineer',
-            'salary'=> 4000000,
-        ]
-    ];
-   $job = Arr::first($jobs, fn($job)=>$job['id']==$id);
+    
+   $job = job::find($id);
     return view('job',[
-        'job'=>$job
+       'job'=> $job
     ]);
 });
 
 Route::get('/contact', function () {
     return view('contact');
 });
+
+// ===============using a decleared array============
+
+// $jobs = [
+//     [
+//         'id'=> 1,
+//         'title'=>'programmer',
+//         'salary'=> 300000,
+//     ],
+//     [
+//         'id'=> 2,
+//         'title'=>'techear',
+//         'salary'=> 3290000,
+//     ],
+//     [
+//         'id'=> 3,
+//         'title'=>'engineer',
+//         'salary'=> 4000000,
+//     ]
+// ];
+
+// Route::get('/', function () {
+//     return view('home');
+// });
+// Route::get('/jobs', function () use($jobs) {
+//     return view('jobs',[
+//         'jobs'=> $jobs
+//     ]);
+// });
+// Route::get('/job/{id}', function ($id) use($jobs) {
+    
+//    $job = Arr::first($jobs, fn($job)=>$job['id']==$id);
+//     return view('job',[
+//        'job'=> $job
+//     ]);
+// });
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
