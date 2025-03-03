935a24027e6fe5e82d3bfeb6b9e4b88b18c17735
Harrison-Muraya <hezronmuraya9@gmail.com>
2025-03-03 10:24:20 +0300

    updated

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -93,7 +93,6 @@
               </svg>
             </button>
           </div>
-
         </div>
       </div>
     </nav>
@@ -104,8 +103,7 @@
       </div>
     </header>
     <main>
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">        
         {{ $slot }}
       </div>
     </main>
